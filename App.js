8deb0e6cfc490be586dbb799589c896183068385
Aitazaz Gilani <aitazazgilani@gmail.com>
2023-05-31 20:33:32 -0600

    Created LoginButton component

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { configureFonts, MD3LightTheme, PaperProvider } from 'react-native-paper';
+import LoginButton from './components/LoginButton';
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working onr</Text>
+      <LoginButton/>
       <StatusBar style="auto" />
     </View>
   );
