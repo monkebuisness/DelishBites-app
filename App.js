c802fe79a5430e5d67d249f7750760120bee4c19
rohannair183 <Rohannair183@gmail.com>
2023-06-03 11:24:13 -0600

    Fixed Merge Edits

--- App.js
+++ App.js
@@ -1,36 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import {
-	configureFonts,
-	MD3LightTheme,
-	PaperProvider,
-} from "react-native-paper";
-import LoginButton from "./components/LoginButton";
-import LoginForum from "./components/LoginForum";
-import CustomSearchBar from "./components/CustomSearchBar";
+import { configureFonts, MD3LightTheme, Provider } from "react-native-paper";
+import { Provider as PaperProvider } from "react-native-paper";
 
+import Alert from "./components/Alert";
 import LoginButton from "./components/LoginButton";
 import LoginForum from "./components/LoginForum";
-import Alert from "./components/Alert";
 import CustomSearchBar from "./components/CustomSearchBar";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
 	return (
 		<Provider>
-			<View style={styles.container}>
-				<CustomSearchBar />
-				<StatusBar style='auto' />
-				<Alert
-					title='Title'
-					text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+			<Alert
+				title='Title'
+				text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
           molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
           numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
           optio, eaque rerum!'
-					button1label='Previous'
-					button2label='Next'
-					button1onPress={() => console.log("Button 1 pressed")}
-					button2onPress={() => console.log("Button 2 pressed")}
-				/>
+				button1label='Previous'
+				button2label='Next'
+				button1onPress={() => console.log("Button 1 pressed")}
+				button2onPress={() => console.log("Button 2 pressed")}
+			/>
+			<LoginPage />
+			<View style={styles.container}>
+				<StatusBar style='auto' />
 			</View>
 		</Provider>
 	);
