969099f3a7ea863d05cc784c9ab979faa44389d3
rohannair183 <Rohannair183@gmail.com>
2023-06-03 22:08:54 -0600

    Fixed Search Bar Colour

--- components/CustomSearchBar.jsx
+++ components/CustomSearchBar.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Searchbar } from "react-native-paper";
-import LinearGradient from "react-native-linear-gradient";
 
 const styles = {
 	container: {
@@ -15,8 +14,7 @@ const styles = {
 		// top: "14.91%",
 		// bottom: "79.6%",
 		width: "95%",
-		backgroundColor:
-			"linear-gradient(0deg, rgba(75, 49, 44, 0.45), rgba(75, 49, 44, 0.45)), #FFFBFE",
+		backgroundColor: "#aca09f",
 
 		borderRadius: 28,
 	},
